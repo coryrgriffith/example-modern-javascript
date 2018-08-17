8a65f22be5cf69bcabbd2f4257efc4c65e4202d1
coryrgriffith <corygriffith9@gmail.com>
2018-08-16 21:05:49 -0500

    add webpack, babel, webpack config file

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 // load math.js
-import * as math from 'mathjs'
+require("babel-polyfill");
+// import * as math from 'mathjs'
 
 var moment = require('moment');
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 // use math.js
-console.log(math.sqrt(-4)); // 2i
+// console.log(math.sqrt(-4)); // 2i
 
-console.log(math.round(math.e, 15));
+// console.log(math.round(math.e, 15));
+
+let fibonacci = {
+  [Symbol.iterator]() {
+    let pre = 0, cur = 1;
+    return {
+      next() {
+        [pre, cur] = [cur, pre + cur];
+        return { done: false, value: cur }
+      }
+    }
+  }
+}
+
+for (var n of fibonacci) {
+  // truncate the sequence at 1000
+  if (n > 1000)
+    break;
+  console.log(n);
+}
