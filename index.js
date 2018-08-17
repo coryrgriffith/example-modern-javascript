2e69f735630064f7fa22e1592e38bae5ddee9402
coryrgriffith <corygriffith9@gmail.com>
2018-08-16 20:27:13 -0500

    add node.js and mathjs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// load math.js
+import * as math from 'mathjs'
+
+var moment = require('moment');
+
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+// use math.js
+console.log(math.sqrt(-4)); // 2i
+
+console.log(math.round(math.e, 15));
